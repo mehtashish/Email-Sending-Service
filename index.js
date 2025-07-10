4bab18ff502f9ab032192d427d679b00cecfd00a
mehtashish <mehta.mtj@gmail.com>
2025-07-10 16:15:37 +0530

    Add. Loop to send multiple emails to test rate limiting

--- index.js
+++ index.js
@@ -3,17 +3,17 @@ const EmailService = require("./src/EmailService");
 (async () => {
   const service = new EmailService();
 
-  const email = {
-    id: "123",
-    to: "test@example.com",
-    subject: "Testing",
-    body: "This is a test email.",
-  };
+  // const email = {
+  //   id: "123",
+  //   to: "test@example.com",
+  //   subject: "Testing",
+  //   body: "This is a test email.",
+  // };
 
   // First attempt for sending email
 
-  const firstResult = await service.sendEmail(email);
-  console.log("Result:", firstResult);
+  // const firstResult = await service.sendEmail(email);
+  // console.log("Result:", firstResult);
 
   // Attempting second email send
   // This attempt will be skiped as the email is already sent
@@ -21,4 +21,16 @@ const EmailService = require("./src/EmailService");
   // console.log("\nSending email again..");
   // const secondResult = await service.sendEmail(email);
   // console.log("Result:", secondResult);
+
+  for (let i = 0; i < 5; i++) {
+    const email = {
+      id: `email-${i}`, // to avoid idempotency block
+      to: "test@example.com",
+      subject: `Testing`,
+      body: `This is a test email.`,
+    };
+
+    const result = await service.sendEmail(email);
+    console.log(`Attempt ${i + 1}:`, result);
+  }
 })();
