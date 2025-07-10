8f3ad1672480a4950900082061d220375866b0bd
mehtashish <mehta.mtj@gmail.com>
2025-07-10 11:59:23 +0530

    Update. Sending duplicate email for idempotency check

--- index.js
+++ index.js
@@ -4,11 +4,21 @@ const EmailService = require("./src/EmailService");
   const service = new EmailService();
 
   const email = {
+    id: "123",
     to: "test@example.com",
     subject: "Testing",
     body: "This is a test email.",
   };
 
-  const result = await service.sendEmail(email);
-  console.log("Final result:", result);
+  // First attempt for sending email
+
+  const firstResult = await service.sendEmail(email);
+  console.log("Result:", firstResult);
+
+  // Attempting second email send
+  // This attempt will be skiped as the email is already sent
+
+  // console.log("\nSending email again..");
+  // const secondResult = await service.sendEmail(email);
+  // console.log("Result:", secondResult);
 })();
